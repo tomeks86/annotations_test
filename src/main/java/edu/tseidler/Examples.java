753ec1244bce9a1f9a0cdefd9154665fc7a7fa02
Tomasz Seidler <tomasz.z.seidler@gmail.com>
2018-05-04 12:47:40 +0200

    cleaning of not used classes

--- src/main/java/edu/tseidler/Examples.java
+++
@@ -1,9 +0,0 @@
-package edu.tseidler;
-
-import java.lang.annotation.*;
-
-@Retention(RetentionPolicy.RUNTIME)
-@Target(ElementType.FIELD)
-public @interface Examples {
-    Example[] value() default {};
-}

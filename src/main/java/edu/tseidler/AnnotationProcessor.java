ee8a1bc08ea903fbb0e4445fda49b7832338220f
Tomasz Seidler <tomasz.z.seidler@gmail.com>
2018-05-04 12:20:44 +0200

    getAnnotationsByType used with repeatable annotation Example instead of checking Exapmles.class and Example.class separately

--- src/main/java/edu/tseidler/AnnotationProcessor.java
+++ src/main/java/edu/tseidler/AnnotationProcessor.java
@@ -1,17 +1,17 @@
 package edu.tseidler;
 
+import java.lang.annotation.Annotation;
 import java.lang.reflect.Field;
 
 public class AnnotationProcessor {
     public static void process() {
         Class cl = Main.class;
         for (Field f : cl.getFields()) {
-            if (f.getAnnotation(Examples.class) != null) {
-                for (Example example : f.getAnnotation(Examples.class).value()) {
-                    System.out.printf("%s: %s%n", f.getName(), example.id());
+            Annotation[] annotations;
+            if ((annotations = f.getAnnotationsByType(Example.class)) != null) {
+                for (Annotation annotation : annotations) {
+                    System.out.printf("%s: %s%n", f.getName(), ((Example) annotation).id());
                 }
-            } else {
-                System.out.printf("%s: %s%n", f.getName(), f.getAnnotation(Example.class).id());
             }
         }
     }

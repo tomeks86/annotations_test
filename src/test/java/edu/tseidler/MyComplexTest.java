3586b490b7eaeb0afe00e0c26cb4ce4c0792640b
Tomasz Seidler <tomasz.z.seidler@gmail.com>
2018-05-04 10:22:49 +0200

    simple unit test with junit4

---
+++ src/test/java/edu/tseidler/MyComplexTest.java
@@ -0,0 +1,21 @@
+package edu.tseidler;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class MyComplexTest {
+    private Complex myNumber;
+
+    @Before
+    public void setUp() {
+        myNumber = new Complex(1, 0);
+    }
+
+    @Test
+    public void shouldAddNumber() {
+        myNumber = myNumber.add(new Complex(1, 1));
+
+        Assert.assertTrue(myNumber.equals(new Complex(2, 1)));
+    }
+}
